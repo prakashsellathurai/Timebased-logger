f5a4f16aef345f0e908174db0eae853f1d091bfd
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-07-21 18:44:16 +0530

    Update setup.py with new author name and project URL

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="timebased_logger",
     version="0.1.0",
     description="A logger that logs messages based on time intervals, not message count.",
-    author="Your Name",
+    author="Prakash Sellathurai",
     packages=find_packages(),
     python_requires=">=3.6",
-    url="https://johnscolaro.xyz/blog/log-by-time-not-by-count",
+    url="https://github.com/prakashsellathurai/Timebased-logger",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
