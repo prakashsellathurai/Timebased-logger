4eb313b09dd82dd8f1b7e1835bd16dd0ad0350ce
Prakash Sellathurai <prakashsellathurai@gmail.com>
2025-07-22 11:54:44 +0530

    Add long description and content type to setup.py for better package documentation

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="timebased_logger",
     version="0.1.0",
     description="A logger that logs messages based on time intervals, not message count.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Prakash Sellathurai",
     packages=find_packages(),
     python_requires=">=3.6",
